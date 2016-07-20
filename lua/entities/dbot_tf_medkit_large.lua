bb9fa2c66108c9f7711179398b1eaa466f4d9921
DBot <roboderpy@gmail.com>
2016-07-20 22:14:06 +0700

    Added pickups

---
+++ lua/entities/dbot_tf_medkit_large.lua
@@ -0,0 +1,26 @@
+
+--[[
+Copyright (C) 2016 DBot
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+
+ENT.Type = 'anim'
+ENT.Base = 'dbot_tf_medkit_base'
+ENT.PrintName = 'Large Medkit'
+ENT.Author = 'DBot'
+ENT.Category = 'DBot'
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+ENT.Model = 'models/items/medkit_large.mdl'
+ENT.Heal = 1
