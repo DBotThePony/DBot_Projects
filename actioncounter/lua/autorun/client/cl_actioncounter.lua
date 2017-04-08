dd067eb9ab709a8878a180e0ac788ef47218a19a
DBot <roboderpy@gmail.com>
2017-04-08 17:05:23 +0700

    Timer

--- actioncounter/lua/autorun/client/cl_actioncounter.lua
+++ actioncounter/lua/autorun/client/cl_actioncounter.lua
@@ -1,110 +1,114 @@
-if not DNotify then
-  print('-------------------------------------------------------')
-  print('-- ACTION COUNTER WAS INSTALLED WITHOUT DNOTIFY LIBRARY')
-  print('-- THIS WILL NOT WORK')
-  print('-------------------------------------------------------')
-  return 
-end
-local HUInMeter = 40
-local DISABLE = CreateConVar('cl_ac_disable', '0', {
-  FCVAR_ARCHIVE
-}, 'Disable action counter display')
-local NetworkedValues = {
-  {
-    'jump',
-    'Jump streak: %s'
-  },
-  {
-    'speed',
-    'Run distance: %sm'
-  },
-  {
-    'duck',
-    'Duck distance: %sm'
-  },
-  {
-    'walk',
-    'Walk distance: %sm'
-  },
-  {
-    'water',
-    'On water distance: %sm'
-  },
-  {
-    'uwater',
-    'Underwater distance: %sm'
-  },
-  {
-    'fall',
-    'Fall distance: %sm'
-  },
-  {
-    'climb',
-    'Climb distance: %sm'
-  },
-  {
-    'height',
-    'Maximal potential height: %sm'
+local init
+init = function()
+  if not DNotify then
+    print('-------------------------------------------------------')
+    print('-- ACTION COUNTER WAS INSTALLED WITHOUT DNOTIFY LIBRARY')
+    print('-- THIS WILL NOT WORK')
+    print('-------------------------------------------------------')
+    return 
+  end
+  local HUInMeter = 40
+  local DISABLE = CreateConVar('cl_ac_disable', '0', {
+    FCVAR_ARCHIVE
+  }, 'Disable action counter display')
+  local NetworkedValues = {
+    {
+      'jump',
+      'Jump streak: %s'
+    },
+    {
+      'speed',
+      'Run distance: %sm'
+    },
+    {
+      'duck',
+      'Duck distance: %sm'
+    },
+    {
+      'walk',
+      'Walk distance: %sm'
+    },
+    {
+      'water',
+      'On water distance: %sm'
+    },
+    {
+      'uwater',
+      'Underwater distance: %sm'
+    },
+    {
+      'fall',
+      'Fall distance: %sm'
+    },
+    {
+      'climb',
+      'Climb distance: %sm'
+    },
+    {
+      'height',
+      'Maximal potential height: %sm'
+    }
   }
-}
-for _index_0 = 1, #NetworkedValues do
-  local nData = NetworkedValues[_index_0]
-  nData.func = function(self)
-    if nData.lastChange > RealTime() - 4 then
-      if nData[1] ~= 'jump' then
-        self:SetText(nData[2]:format(math.floor(nData.networkValue / HUInMeter * 10) / 10))
-      else
-        self:SetText(nData[2]:format(nData.networkValue))
-      end
-      return self:ExtendTimer()
-    end
-  end
-end
-local NET
-NET = function()
-  if DISABLE:GetBool() then
-    return 
-  end
-  for _index_0 = 1, #NetworkedValues do
-    local _continue_0 = false
-    repeat
-      local nData = NetworkedValues[_index_0]
-      local readValue = net.ReadUInt(32)
-      if readValue == 0 then
-        _continue_0 = true
-        break
-      end
-      nData.networkValue = nData.networkValue or readValue
-      local changed = nData.networkValue ~= readValue
-      nData.networkValue = readValue
-      if changed then
-        nData.lastChange = RealTime()
-      end
-      _continue_0 = true
-    until true
-    if not _continue_0 then
-      break
-    end
-  end
-end
-local Think
-Think = function()
-  if DISABLE:GetBool() then
-    return 
-  end
-  local ctime = RealTime() - 4
   for _index_0 = 1, #NetworkedValues do
     local nData = NetworkedValues[_index_0]
-    if nData.lastChange and nData.lastChange > ctime then
-      if not nData.notif or not nData.notif:IsValid() then
-        nData.notif = DNotify.CreateSlide()
-        nData.notif:SetThink(nData.func)
-        nData.notif:SetNotifyInConsole(false)
-        nData.notif:Start()
-        nData.notif:Think()
+    nData.func = function(self)
+      if nData.lastChange > RealTime() - 4 then
+        if nData[1] ~= 'jump' then
+          self:SetText(nData[2]:format(math.floor(nData.networkValue / HUInMeter * 10) / 10))
+        else
+          self:SetText(nData[2]:format(nData.networkValue))
+        end
+        return self:ExtendTimer()
       end
     end
   end
+  local NET
+  NET = function()
+    if DISABLE:GetBool() then
+      return 
+    end
+    for _index_0 = 1, #NetworkedValues do
+      local _continue_0 = false
+      repeat
+        local nData = NetworkedValues[_index_0]
+        local readValue = net.ReadUInt(32)
+        if readValue == 0 then
+          _continue_0 = true
+          break
+        end
+        nData.networkValue = nData.networkValue or readValue
+        local changed = nData.networkValue ~= readValue
+        nData.networkValue = readValue
+        if changed then
+          nData.lastChange = RealTime()
+        end
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+  end
+  local Think
+  Think = function()
+    if DISABLE:GetBool() then
+      return 
+    end
+    local ctime = RealTime() - 4
+    for _index_0 = 1, #NetworkedValues do
+      local nData = NetworkedValues[_index_0]
+      if nData.lastChange and nData.lastChange > ctime then
+        if not nData.notif or not nData.notif:IsValid() then
+          nData.notif = DNotify.CreateSlide()
+          nData.notif:SetThink(nData.func)
+          nData.notif:SetNotifyInConsole(false)
+          nData.notif:Start()
+          nData.notif:Think()
+        end
+      end
+    end
+  end
+  hook.Add('Think', 'DActionCounter', Think)
+  return net.Receive('dactioncounter_network', NET)
 end
-hook.Add('Think', 'DActionCounter', Think)
-net.Receive('dactioncounter_network', NET)
+timer.Simple(0, init)
--- actioncounter/moon/autorun/client/cl_actioncounter.moon
+++ actioncounter/moon/autorun/client/cl_actioncounter.moon
@@ -15,65 +15,68 @@
 -- limitations under the License.
 -- 
 
-if not DNotify
-	print('-------------------------------------------------------')
-	print('-- ACTION COUNTER WAS INSTALLED WITHOUT DNOTIFY LIBRARY')
-	print('-- THIS WILL NOT WORK')
-	print('-------------------------------------------------------')
-	return
+init = ->
+	if not DNotify
+		print('-------------------------------------------------------')
+		print('-- ACTION COUNTER WAS INSTALLED WITHOUT DNOTIFY LIBRARY')
+		print('-- THIS WILL NOT WORK')
+		print('-------------------------------------------------------')
+		return
 
-HUInMeter = 40
-DISABLE = CreateConVar('cl_ac_disable', '0', {FCVAR_ARCHIVE}, 'Disable action counter display')
+	HUInMeter = 40
+	DISABLE = CreateConVar('cl_ac_disable', '0', {FCVAR_ARCHIVE}, 'Disable action counter display')
 
-NetworkedValues = {
-	{'jump', 'Jump streak: %s'}
-	{'speed', 'Run distance: %sm'}
-	{'duck', 'Duck distance: %sm'}
-	{'walk', 'Walk distance: %sm'}
-	{'water', 'On water distance: %sm'}
-	{'uwater', 'Underwater distance: %sm'}
-	{'fall', 'Fall distance: %sm'}
-	{'climb', 'Climb distance: %sm'}
-	{'height', 'Maximal potential height: %sm'}
-}
+	NetworkedValues = {
+		{'jump', 'Jump streak: %s'}
+		{'speed', 'Run distance: %sm'}
+		{'duck', 'Duck distance: %sm'}
+		{'walk', 'Walk distance: %sm'}
+		{'water', 'On water distance: %sm'}
+		{'uwater', 'Underwater distance: %sm'}
+		{'fall', 'Fall distance: %sm'}
+		{'climb', 'Climb distance: %sm'}
+		{'height', 'Maximal potential height: %sm'}
+	}
 
-for nData in *NetworkedValues
-	nData.func = =>
-		if nData.lastChange > RealTime! - 4
-			if nData[1] ~= 'jump'
-				@SetText(nData[2]\format(math.floor(nData.networkValue / HUInMeter * 10) / 10))
-			else
-				@SetText(nData[2]\format(nData.networkValue))
-				
-			@ExtendTimer!
-
-NET = ->
-	if DISABLE\GetBool() then return
 	for nData in *NetworkedValues
-		readValue = net.ReadUInt(32)
-		if readValue == 0 then continue
-		nData.networkValue = nData.networkValue or readValue
-		changed = nData.networkValue ~= readValue
-		nData.networkValue = readValue
+		nData.func = =>
+			if nData.lastChange > RealTime! - 4
+				if nData[1] ~= 'jump'
+					@SetText(nData[2]\format(math.floor(nData.networkValue / HUInMeter * 10) / 10))
+				else
+					@SetText(nData[2]\format(nData.networkValue))
+					
+				@ExtendTimer!
+
+	NET = ->
+		if DISABLE\GetBool() then return
+		for nData in *NetworkedValues
+			readValue = net.ReadUInt(32)
+			if readValue == 0 then continue
+			nData.networkValue = nData.networkValue or readValue
+			changed = nData.networkValue ~= readValue
+			nData.networkValue = readValue
+			
+			if changed
+				nData.lastChange = RealTime!
+
+	Think = ->
+		if DISABLE\GetBool() then return
+		ctime = RealTime! - 4
 		
-		if changed
-			nData.lastChange = RealTime!
+		for nData in *NetworkedValues
+			if nData.lastChange and nData.lastChange > ctime
+				if not nData.notif or not nData.notif\IsValid!
+					nData.notif = DNotify.CreateSlide()
+					nData.notif\SetThink(nData.func)
+					nData.notif\SetNotifyInConsole(false)
+					nData.notif\Start()
+					nData.notif\Think()
+					
+		
+	hook.Add('Think', 'DActionCounter', Think)
+	net.Receive('dactioncounter_network', NET)
 
-Think = ->
-	if DISABLE\GetBool() then return
-	ctime = RealTime! - 4
-	
-	for nData in *NetworkedValues
-		if nData.lastChange and nData.lastChange > ctime
-			if not nData.notif or not nData.notif\IsValid!
-				nData.notif = DNotify.CreateSlide()
-				nData.notif\SetThink(nData.func)
-				nData.notif\SetNotifyInConsole(false)
-				nData.notif\Start()
-				nData.notif\Think()
-				
-	
-hook.Add('Think', 'DActionCounter', Think)
-net.Receive('dactioncounter_network', NET)
+timer.Simple(0, init)
 
 return
