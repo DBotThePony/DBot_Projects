c528a52c50b855fe708c028a87f7e018b62b6fc2
DBot <roboderpy@gmail.com>
2017-05-31 07:04:19 +0700

    Heavy, Pyro and Soldier shotguns

---
+++ lua/weapons/dbot_tf_shotgun_heavy.lua
@@ -0,0 +1,10 @@
+local BaseClass = baseclass.Get('dbot_tf_shotgun')
+SWEP.Base = 'dbot_tf_shotgun'
+SWEP.Author = 'DBot'
+SWEP.Category = 'TF2'
+SWEP.PrintName = 'Heavyweapons Shotgun'
+SWEP.ViewModel = 'models/weapons/v_models/v_shotgun_heavy.mdl'
+SWEP.WorldModel = 'models/weapons/w_models/w_shotgun.mdl'
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.AdminOnly = false
