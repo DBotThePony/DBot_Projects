ff44ac29f39620cacd66a1271b4e8e2d90ed6834
DBot <roboderpy@gmail.com>
2017-06-19 18:37:25 +0700

    Rewrite of php script

--- scripts/get_pack.php
+++ scripts/get_pack.php
@@ -17,138 +17,176 @@
 //
 
 function dummy() {
-	$Seperator = '----______VLL_PACK_FILE_SEPERATOR______----';
+    $Seperator = '----______VLL_PACK_FILE_SEPERATOR______----';
 
-	header('Content-Type: text/plain; charset=utf8');
+    header('Content-Type: text/plain; charset=utf8');
 
-	$Bundle = $_GET['bundle'];
-	if (!$Bundle)
-		die('No bundle!');
+    $Bundle = $_GET['bundle'];
+    if (!$Bundle) die('No bundle!');
 
-	if (strstr($Bundle, '.'))
-		die('Hack');
+    if (strstr($Bundle, '.')) die('Hack');
+    if (strstr($Bundle, '/')) die('Hack');
 
-	$Blacklist = [
-		'luapad',
-		'gma',
-		'git',
-		'github',
-		'wcache',
-		'bundlecache',
-		'cache',
-	];
+    $Blacklist = [
+        'luapad',
+        'gma',
+        'git',
+        'github',
+        'wcache',
+        'bundlecache',
+        'cache',
+    ];
 
-	$Cache = [
-		'neurotec',
-		'server_playermodels',
-		'server_cars',
-	];
+    foreach ($Blacklist as $value)
+        if (strpos($Bundle, $value) !== false) die('Hack');
 
-	foreach ($Blacklist as $value)
-		if (strpos($Bundle, $value) !== false)
-			die('Hack');
+    function Search($path) {
+        $readFiles = [];
 
-	$Files = [];
+        foreach (scandir($path) as $value) {
+            if ($value[0] === '.') continue;
+            
+            if (is_dir($path . '/' . $value)) {
+                $findFiles = Search($path . '/' . $value);
 
-	function Search($path)
-	{
-		global $Files;
-		$Rep = scandir($path);
-		
-		foreach ($Rep as $value)
-		{
-			if ($value[0] === '.') continue;
-			
-			if (is_dir($path . '/' . $value))
-				Search($path . '/' . $value);
-			else
-			{
-				if (strpos($value, '.lua') === false) continue;
-				array_push($Files, $path . '/' . $value);
-			}
-		}
-	}
+                foreach ($findFiles as $fil) {
+                    array_push($readFiles, $fil);
+                }
+            } else {
+                if (strpos($value, '.lua') === false) continue;
+                array_push($readFiles, $path . '/' . $value);
+            }
+        }
 
-	if (is_file('./luapad/' . $Bundle . '.lua'))
-	{
-		$output = '';
-		$output = $output . 'autorun/' . $Bundle . '.lua' . $Seperator;
-		$output = $output . file_get_contents('./luapad/' . $Bundle . '.lua');
-		$output = $output . $Seperator;
-		echo $output;
-		exit;
-	}
+        return $readFiles;
+    }
 
-	$metap = 'cache/meta/' . $Bundle;
-	$lockfile = 'cache/' . $Bundle . '.lock';
+    if (is_file('./luapad/' . $Bundle . '.lua')) {
+        $output = '';
+        $output = $output . 'autorun/' . $Bundle . '.lua' . $Seperator;
+        $output = $output . file_get_contents('./luapad/' . $Bundle . '.lua');
+        $output = $output . $Seperator;
+        echo $output;
+        exit;
+    }
 
-	//if (is_file($lockfile))
-		//sleep(1);
+    $metap = 'cache/meta/' . $Bundle;
+    $LockFileName = 'cache/' . $Bundle . '.lock';
 
-	$cachep = 'cache/build/' . $Bundle . '.txt';
-	$CTime = time();
-	$Locked = false;
-	$RebuildFileCache = !is_file($metap) or filemtime($metap) + 120 < $CTime;
+    if (is_file($LockFileName))
+        sleep(1);
 
-	if ($RebuildFileCache) {
-		$Locked = true;
-		
-		file_put_contents($lockfile, '');
-		if (is_dir('./' . $Bundle)) {
-			Search('./' . $Bundle);
-			$len = strlen($Bundle);
-		} elseif (is_dir('./git/' . $Bundle)) {
-			Search('./git/' . $Bundle . '/lua');
-			$len = strlen('./git/' . $Bundle . '/lua') - 2;
-		} elseif (is_dir('./git/dbot_projects/' . $Bundle)) {
-			Search('./git/dbot_projects/' . $Bundle . '/lua');
-			$len = strlen('./git/dbot_projects/' . $Bundle . '/lua') - 2;
-		}
-		
-		$OutputJSON = [];
-		
-		$CacheFileTime = filemtime($cachep);
-		
-		foreach ($Files as $value) {
-			$CurrFileTime = filemtime($value);
-			
-			if ($CurrFileTime > $CacheFileTime)
-				$RebuildFileCache = true;
-			
-			array_push($OutputJSON, [
-				'file' => $value,
-				'pfile' => substr($value, 3 + $len),
-				'stamp' => $CurrFileTime,
-			]);
-		}
-		
-		// file_put_contents($metap, json_encode($OutputJSON));
-		
-		$Locked = true;
-		
-		//file_put_contents($lockfile, '');
-		$output = '';
-		
-		foreach ($OutputJSON as $val) {
-			$output = $output . $val['pfile'] . $Seperator;
-			$output = $output . file_get_contents($val['file']);
-			$output = $output . $Seperator;
-		}
-		
-		file_put_contents($cachep, $output);
-		
-		if (is_file($cachep . '.gz'))
-			unlink($cachep . '.gz');
-		
-		// exec('"C:\\Program Files\\Git\\bin\\gzip" "' . getcwd() . '/' . $cachep . '"');
-		file_put_contents($cachep, $output);
-		
-		$Locked = false;
-		//unlink($lockfile);
-		//sleep(0.2);
-	}
+    $cachep = 'cache/build/' . $Bundle . '.txt';
+    $CTime = time();
+    $Locked = false;
+    $RebuildMetaCache = !is_file($metap);
+    $RebuildFileCache = !is_file($cachep) or filemtime($cachep) + 3600 < $CTime;
+    $jsonMeta = [];
 
-	header('Location: ' . $cachep);
+    $Files = [];
+    $BundleLen = 0;
+
+    if (is_dir('./' . $Bundle)) {
+        $Files = Search('./' . $Bundle);
+        $BundleLen = strlen($Bundle);
+    } elseif (is_dir('./git/' . $Bundle)) {
+        $Files = Search('./git/' . $Bundle . '/lua');
+        $BundleLen = strlen('./git/' . $Bundle . '/lua') - 2;
+    } elseif (is_dir('./git/dbot_projects/' . $Bundle)) {
+        $Files = Search('./git/dbot_projects/' . $Bundle . '/lua');
+        $BundleLen = strlen('./git/dbot_projects/' . $Bundle . '/lua') - 2;
+    }
+
+    function rebuildMeta($LockFileName, $Files, $RebuildFileCache, $RebuildMetaCache, $metap, $cachep, $BundleLen) {
+        file_put_contents($LockFileName, '');
+
+        $OutputJSON = [];
+        $RebuildFileCache = true;
+        
+        foreach ($Files as $value) {
+            array_push($OutputJSON, [
+                'file' => $value,
+                'pfile' => substr($value, 3 + $BundleLen),
+                'stamp' => filemtime($value),
+            ]);
+        }
+        
+        file_put_contents($metap, json_encode($OutputJSON));
+        unlink($LockFileName);
+        return $OutputJSON;
+    }
+
+    if ($RebuildMetaCache) {
+        $readMeta = rebuildMeta($LockFileName, $Files, $RebuildFileCache, $RebuildMetaCache, $metap, $cachep, $BundleLen);
+        $RebuildMetaCache = false;
+    } else {
+        $metaFiles = file_get_contents($metap);
+        $jsonMeta = json_decode($metaFiles, true);
+
+        foreach ($jsonMeta as $val) {
+            if (!is_file($val['file'])) {
+                $jsonMeta = rebuildMeta($LockFileName, $Files, $RebuildFileCache, $RebuildMetaCache, $metap, $cachep, $BundleLen);
+                $RebuildMetaCache = false;
+                break;
+            }
+        }
+
+        $readMeta = $jsonMeta;
+    }
+
+    function rebuildFileCache($LockFileName, $readMeta, $cachep) {
+        file_put_contents($LockFileName, '');
+        $output = '';
+        
+        foreach ($readMeta as $val) {
+            $output = $output . $val['pfile'] . $Seperator;
+            $output = $output . file_get_contents($val['file']);
+            $output = $output . $Seperator;
+        }
+
+        file_put_contents($cachep, $output);
+        unlink($LockFileName);
+    }
+
+    if (!$RebuildFileCache) {
+        foreach ($readMeta as $val) {
+            $hit = false;
+
+            foreach ($Files as $val2) {
+                if ($val2 === $val['file']) {
+                    $hit = true;
+                    break;
+                }
+            }
+
+            if (!$hit) {
+                rebuildMeta();
+                break;
+            }
+        }
+        
+        foreach ($Files as $val2) {
+            $hit = false;
+
+            foreach ($readMeta as $val) {
+                if ($val2 === $val['file']) {
+                    $hit = true;
+                    break;
+                }
+            }
+
+            if (!$hit) {
+                rebuildMeta();
+                break;
+            }
+        }
+    }
+
+    if ($RebuildFileCache) {
+        rebuildFileCache($LockFileName, $readMeta, $cachep);
+    }
+
+    header('Location: ' . $cachep);
 }
 
 dummy();
