aef6a975cd121c614a55cf8916a9a5860fe96e40
DBot <roboderpy@gmail.com>
2017-09-07 07:26:30 +0700

    Added Buildables scripts

---
+++ tf2buildables.bat
@@ -0,0 +1,2 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe tf2buildables
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1129601162 -addon tf2buildables.gma -icon tf2buildables.jpg -changes "See https://git.dbot.serealia.ca/dbot/dbot_projects"
