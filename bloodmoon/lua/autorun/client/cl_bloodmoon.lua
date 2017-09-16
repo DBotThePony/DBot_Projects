a39107209fbcc9354fb340a5ea48e2c9d08d5615
DBot <roboderpy@gmail.com>
2017-09-16 18:27:39 +0700

    clean this fukkin' up

--- bloodmoon/lua/autorun/client/cl_bloodmoon.lua
+++ bloodmoon/lua/autorun/client/cl_bloodmoon.lua
@@ -1,112 +1,133 @@
 
---[[
-Copyright (C) 2015 DBot
+-- Copyright (C) 2015-2017 DBot
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-]]
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+local BLACK = Color(0, 0, 0)
 
 local function Start()
 	if IsValid(BloodMoonSound) then
 		BloodMoonSound:Stop()
 	end
-	sound.PlayURL("http://80.83.200.79/music_repository/terraria_ost_flac_1408392145/Terraria%20Original%20Soundtrack/Eerie.ogg","",function(channel)
+
+	sound.PlayURL("http://80.83.200.79/music_repository/terraria_ost_flac_1408392145/Terraria%20Original%20Soundtrack/Eerie.ogg", "", function(channel)
 		BloodMoonSound = channel
 	end)
-	chat.AddText(Color(119,205,106),"Blood Moon is rising...")
+
+	chat.AddText(Color(119, 205, 106), "The Blood Moon is rising...")
 end
 
 local function StartEclipse()
 	if IsValid(BloodMoonSound) then
 		BloodMoonSound:Stop()
 	end
-	sound.PlayURL("http://80.83.200.79/music_repository/terraria_volumetwo_flac_1408392145/Terraria%20Original%20Soundtrack%20Vol.%202/Eclipse.ogg","",function(channel)
+
+	sound.PlayURL("http://80.83.200.79/music_repository/terraria_volumetwo_flac_1408392145/Terraria%20Original%20Soundtrack%20Vol.%202/Eclipse.ogg", "", function(channel)
 		BloodMoonSound = channel
 	end)
-	chat.AddText(Color(119,205,106),"A solar eclipse is happening!")
+
+	chat.AddText(Color(119, 205, 106), "A solar eclipse is happening!")
 end
 
 local function End()
 	if IsValid(BloodMoonSound) then
 		BloodMoonSound:Stop()
 	end
-	chat.AddText(Color(119,205,106),"Blood Moon ended!")
+
+	chat.AddText(Color(119, 205, 106), "Blood Moon has ended!")
 end
 
 local function EndEclipse()
 	if IsValid(BloodMoonSound) then
 		BloodMoonSound:Stop()
 	end
-	chat.AddText(Color(119,205,106),"A solar eclipse has ended!")
+
+	chat.AddText(Color(119, 205, 106), "A solar eclipse has ended!")
 end
 
-net.Receive("BloodMoon",function()
+net.Receive("DBot.BloodMoon", function()
 	local action = net.ReadBool()
 	if action then Start() else End() end
 end)
 
-net.Receive("Eclipse",function()
+net.Receive("DBot.Eclipse", function()
 	local action = net.ReadBool()
 	if action then StartEclipse() else EndEclipse() end
 end)
 
 local fogstart = 100
 local fogend = 750
-hook.Add("SetupSkyboxFog","BloodMoon",function(scale)
-	if !GetGlobalBool("BloodMoon") then return end
-	render.FogMode( 1 ) 
-	render.FogStart( fogstart*scale )
-	render.FogEnd( fogend*scale )
-	render.FogMaxDensity( 1 )
-	render.FogColor( 20,0,0 )
+
+local function SetupSkyboxFog(scale)
+	if not GetGlobalBool("DBot.BloodMoon") then return end
+	render.FogMode(1)
+	render.FogStart(fogstart*scale)
+	render.FogEnd(fogend*scale)
+	render.FogMaxDensity(1)
+	render.FogColor(20, 0, 0)
 	return true
-end)
-hook.Add("SetupWorldFog","BloodMoon",function(scale)
-	if !GetGlobalBool("BloodMoon") then return end
-	render.FogMode( 1 ) 
-	render.FogStart( fogstart )
-	render.FogEnd( fogend )
-	render.FogMaxDensity( 1 )
-	render.FogColor( 20,0,0 )
+end
+
+local function SetupWorldFog(scale)
+	if not GetGlobalBool("DBot.BloodMoon") then return end
+	render.FogMode(1)
+	render.FogStart(fogstart)
+	render.FogEnd(fogend)
+	render.FogMaxDensity(1)
+	render.FogColor(20, 0, 0)
 	return true
-end)
-hook.Add( "PostDraw2DSkyBox", "BloodMoon", function()
-	if !(GetGlobalBool("BloodMoon") or GetGlobalBool("Eclipse")) then return end
-	render.OverrideDepthEnable( true, false )
-	cam.Start3D( Vector( 0, 0, 0 ), EyeAngles() )
-		render.DrawQuadEasy( Vector(0, 0,10), Vector( 0, 0, -1 ), 512, 512, Color( 0, 0, 0 ), 0 )
-		render.DrawQuadEasy( Vector(64, 64,0), Vector( -1, 0, 0 ), 512, 512, Color( 0, 0, 0 ), 0 )
-		render.DrawQuadEasy( Vector(-64, 64,0), Vector( 0, -1, 0 ), 512, 512, Color( 0, 0, 0 ), 0 )
-		render.DrawQuadEasy( Vector(-64, -64,0), Vector( 1, 0, -1 ), 512, 512, Color( 0, 0, 0 ), 0 )
-		render.DrawQuadEasy( Vector(64, -64,0), Vector( 0, 1, 0 ), 512, 512, Color( 0, 0, 0 ), 0 )
+end
+
+local function PostDraw2DSkyBox()
+	if not GetGlobalBool("DBot.BloodMoon") and not GetGlobalBool("DBot.Eclipse") then return end
+
+	render.OverrideDepthEnable(true, false)
+
+	cam.Start3D(Vector(0, 0, 0), EyeAngles())
+	render.DrawQuadEasy(Vector(0, 0, 10), Vector(0, 0, -1), 512, 512, BLACK, 0)
+	render.DrawQuadEasy(Vector(64, 64, 0), Vector(-1, 0, 0), 512, 512, BLACK, 0)
+	render.DrawQuadEasy(Vector(-64, 64, 0), Vector(0, -1, 0), 512, 512, BLACK, 0)
+	render.DrawQuadEasy(Vector(-64, -64, 0), Vector(1, 0, -1), 512, 512, BLACK, 0)
+	render.DrawQuadEasy(Vector(64, -64, 0), Vector(0, 1, 0), 512, 512, BLACK, 0)
 	cam.End3D()
-	render.OverrideDepthEnable( false, false )
-end)
+
+	render.OverrideDepthEnable(false, false)
+end
+
+hook.Add("SetupSkyboxFog", "DBot.BloodMoon", SetupSkyboxFog)
+hook.Add("SetupWorldFog", "DBot.BloodMoon", SetupWorldFog)
+hook.Add("PostDraw2DSkyBox", "BloodMoon", PostDraw2DSkyBox)
 
 local fogstart = 100
 local fogend = 500
-hook.Add("SetupSkyboxFog","Eclipse",function(scale)
-	if !GetGlobalBool("Eclipse") then return end
-	render.FogMode( 1 ) 
-	render.FogStart( fogstart*scale )
-	render.FogEnd( fogend*scale )
-	render.FogMaxDensity( 1 )
-	render.FogColor( 0,0,0 )
+
+local function SetupWorldFog(scale)
+	if not GetGlobalBool("DBot.Eclipse") then return end
+	render.FogMode(1)
+	render.FogStart(fogstart)
+	render.FogEnd(fogend)
+	render.FogMaxDensity(1)
+	render.FogColor(0, 0, 0)
 	return true
-end)
-hook.Add("SetupWorldFog","Eclipse",function(scale)
-	if !GetGlobalBool("Eclipse") then return end
-	render.FogMode( 1 ) 
-	render.FogStart( fogstart )
-	render.FogEnd( fogend )
-	render.FogMaxDensity( 1 )
-	render.FogColor( 0,0,0 )
+end
+
+local function SetupSkyboxFog(scale)
+	if not GetGlobalBool("DBot.Eclipse") then return end
+	render.FogMode(1)
+	render.FogStart(fogstart*scale)
+	render.FogEnd(fogend*scale)
+	render.FogMaxDensity(1)
+	render.FogColor(0, 0, 0)
 	return true
-end)
+end
+
+hook.Add("SetupSkyboxFog", "DBot.Eclipse", SetupSkyboxFog)
+hook.Add("SetupWorldFog", "DBot.Eclipse", SetupWorldFog)
--- bloodmoon/lua/autorun/server/sv_bloodmoon.lua
+++ bloodmoon/lua/autorun/server/sv_bloodmoon.lua
@@ -1,121 +1,123 @@
 
---[[
-Copyright (C) 2015 DBot
+-- Copyright (C) 2015-2017 DBot
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-]]
+-- This program is distributed in the hope that it will be useful, 
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
 
-util.AddNetworkString("BloodMoon")
-util.AddNetworkString("Eclipse")
+util.AddNetworkString("DBot.BloodMoon")
+util.AddNetworkString("DBot.Eclipse")
 
-CreateConVar("sv_dbot_bloodmoon_chance",10000,{FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},"Chance of happening bloodmoon")
-CreateConVar("sv_dbot_eclipse_chance",30000,{FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},"Chance of happening eclipse")
+CreateConVar("sv_dbot_bloodmoon_chance", 10000, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE}, "Chance of happening bloodmoon")
+CreateConVar("sv_dbot_eclipse_chance", 30000, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE}, "Chance of happening eclipse")
 
 BloodMoon = BloodMoon or {}
 BloodMoon.Death = BloodMoon.Death or 0
 function BloodMoon.Start()
-	if GetGlobalBool("BloodMoon") then return end
-	if GetGlobalBool("Eclipse") then return end
-	SetGlobalBool("BloodMoon",true)
-	net.Start("BloodMoon")
+	if GetGlobalBool("DBot.BloodMoon") then return end
+	if GetGlobalBool("DBot.Eclipse") then return end
+	SetGlobalBool("DBot.BloodMoon", true)
+	net.Start("DBot.BloodMoon")
 	net.WriteBool(true)
 	net.Broadcast()
 end
 
 concommand.Add("bloodmoon", function(ply) 
-	if IsValid(ply) and !ply:IsAdmin() then return end
-	if !GetGlobalBool("BloodMoon") then BloodMoon.Start() else BloodMoon.End() end
-end,nil,"Starts/Ends bloodmoon")
+	if IsValid(ply) and not ply:IsAdmin() then return end
+	if not GetGlobalBool("DBot.BloodMoon") then BloodMoon.Start() else BloodMoon.End() end
+end, nil, "Starts/Ends bloodmoon")
 
 concommand.Add("eclipse", function(ply) 
-	if IsValid(ply) and !ply:IsAdmin() then return end
-	if !GetGlobalBool("Eclipse") then BloodMoon.StartEclipse() else BloodMoon.EndEclipse() end
-end,nil,"Starts/Ends Eclipse")
+	if IsValid(ply) and not ply:IsAdmin() then return end
+	if not GetGlobalBool("DBot.Eclipse") then BloodMoon.StartEclipse() else BloodMoon.EndEclipse() end
+end, nil, "Starts/Ends Eclipse")
 
 function BloodMoon.End()
-	if !GetGlobalBool("BloodMoon") then return end
-	SetGlobalBool("BloodMoon",false)
-	net.Start("BloodMoon")
+	if not GetGlobalBool("DBot.BloodMoon") then return end
+	SetGlobalBool("DBot.BloodMoon", false)
+	net.Start("DBot.BloodMoon")
 	net.WriteBool(false)
 	net.Broadcast()
-	for _,t in pairs(ents.GetAll()) do
+	for _, t in pairs(ents.GetAll()) do
 		if t.BloodMoonZombie then SafeRemoveEntity(t) end
 	end
 end
 
 function BloodMoon.StartEclipse()
-	if GetGlobalBool("BloodMoon") then return end
-	if GetGlobalBool("Eclipse") then return end
-	SetGlobalBool("Eclipse",true)
-	net.Start("Eclipse")
+	if GetGlobalBool("DBot.BloodMoon") then return end
+	if GetGlobalBool("DBot.Eclipse") then return end
+	SetGlobalBool("DBot.Eclipse", true)
+	net.Start("DBot.Eclipse")
 	net.WriteBool(true)
 	net.Broadcast()
 end
 
 function BloodMoon.EndEclipse()
-	if !GetGlobalBool("Eclipse") then return end
-	SetGlobalBool("Eclipse",false)
-	net.Start("Eclipse")
+	if not GetGlobalBool("DBot.Eclipse") then return end
+	SetGlobalBool("DBot.Eclipse", false)
+	net.Start("DBot.Eclipse")
 	net.WriteBool(false)
 	net.Broadcast()
-	for _,t in pairs(ents.GetAll()) do
+	for _, t in pairs(ents.GetAll()) do
 		if t.EclipseZombie then SafeRemoveEntity(t) end
 	end
 end
 
-local ztypes = {"npc_zombie","npc_zombine","npc_headcrab"}
-if !IsMounted("ep2") then
-	ztypes = {"npc_zombie","npc_headcrab_fast","npc_headcrab"}
+local ztypes = {"npc_zombie", "npc_zombine", "npc_headcrab"}
+
+if not IsMounted("ep2") then
+	ztypes = {"npc_zombie", "npc_headcrab_fast", "npc_headcrab"}
 end
-timer.Create("BloodMoon",5,0,function()
-	if !GetGlobalBool("BloodMoon") then return end
+
+timer.Create("BloodMoon", 5, 0, function()
+	if not GetGlobalBool("BloodMoon") then return end
 	
 	local zombies = 0
-	for _,t in pairs(ztypes) do
+	for _, t in pairs(ztypes) do
 		zombies = zombies + #ents.FindByClass(t)
 	end
 	
 	if zombies < 200 then
-		for _,ply in pairs(player.GetAll()) do
+		for _, ply in pairs(player.GetAll()) do
 			local ent = ents.Create(table.Random(ztypes))
 			local pos = ply:GetPos()
-			local newpos = pos+Vector(math.random(-500,500),math.random(-500,500),math.random(-20,20))
+			local newpos = pos+Vector(math.random(-500, 500), math.random(-500, 500), math.random(-20, 20))
 			ent:SetPos(newpos)
 			ent:Spawn()
-			ent:SetAngles(Angle(0,(pos-newpos):Angle().y,0))
+			ent:SetAngles(Angle(0, (pos-newpos):Angle().y, 0))
 			ent.BloodMoonZombie = true
 		end
 	end
 end)
 
-local ztypes = {"npc_zombie","npc_zombine","npc_poisonzombie"}
-if !IsMounted("ep2") then
-	ztypes = {"npc_zombie","npc_poisonzombie"}
+local ztypes = {"npc_zombie", "npc_zombine", "npc_poisonzombie"}
+
+if not IsMounted("ep2") then
+	ztypes = {"npc_zombie", "npc_poisonzombie"}
 end
-timer.Create("Eclipse",3,0,function()
-	if !GetGlobalBool("Eclipse") then return end
+
+timer.Create("Eclipse", 3, 0, function()
+	if not GetGlobalBool("Eclipse") then return end
 	
 	local zombies = 0
-	for _,t in pairs(ztypes) do
+	for _, t in pairs(ztypes) do
 		zombies = zombies + #ents.FindByClass(t)
 	end
 	
 	if zombies < 200 then
-		for _,ply in pairs(player.GetAll()) do
+		for _, ply in pairs(player.GetAll()) do
 			local ent = ents.Create(table.Random(ztypes))
 			local pos = ply:GetPos()
-			local newpos = pos+Vector(math.random(-500,500),math.random(-500,500),math.random(-20,20))
+			local newpos = pos+Vector(math.random(-500, 500), math.random(-500, 500), math.random(-20, 20))
 			ent:SetPos(newpos)
 			ent:Spawn()
-			ent:SetAngles(Angle(0,(pos-newpos):Angle().y,0))
+			ent:SetAngles(Angle(0, (pos-newpos):Angle().y, 0))
 			ent:SetMaxHealth(ent:Health()*3)
 			ent:SetHealth(ent:Health()*3)
 			ent.EclipseZombie = true
@@ -123,32 +125,35 @@ timer.Create("Eclipse",3,0,function()
 	end
 end)
 
-hook.Add("PlayerDeath","BloodMoon",function(ply)
+hook.Add("PlayerDeath", "BloodMoon", function(ply)
 	BloodMoon.Death = BloodMoon.Death + 1
+
 	if BloodMoon.Death > 5 then 
-		if math.random(1,HandleMoonChance()-BloodMoon.Death*200) == 1 then
+		if math.random(1, HandleMoonChance() - BloodMoon.Death * 200) == 1 then
 			BloodMoon.Start()
-			timer.Simple(math.random(120,360),function()
+
+			timer.Simple(math.random(120, 360), function()
 				BloodMoon.End()
 			end)
 		end
 	end
 end)
 
-timer.Create("BloodMoonDeath",30,0,function()
-	BloodMoon.Death = math.max(BloodMoon.Death - 1,0)
+timer.Create("BloodMoonDeath", 30, 0, function()
+	BloodMoon.Death = math.max(BloodMoon.Death - 1, 0)
 end)
 
-timer.Create("BloodMoonEvent",1,0,function()
-	if math.random(1,GetConVar("sv_dbot_bloodmoon_chance"):GetInt()) == 1 then
+timer.Create("BloodMoonEvent", 1, 0, function()
+	if math.random(1, GetConVar("sv_dbot_bloodmoon_chance"):GetInt()) == 1 then
 		BloodMoon.Start()
-		timer.Simple(math.random(120,360),function()
+		timer.Simple(math.random(120, 360), function()
 			BloodMoon.End()
 		end)
 	end
-	if math.random(1,GetConVar("sv_dbot_eclipse_chance"):GetInt()) == 1 then
+
+	if math.random(1, GetConVar("sv_dbot_eclipse_chance"):GetInt()) == 1 then
 		BloodMoon.StartEclipse()
-		timer.Simple(math.random(60,120),function()
+		timer.Simple(math.random(60, 120), function()
 			BloodMoon.EndEclipse()
 		end)
 	end
