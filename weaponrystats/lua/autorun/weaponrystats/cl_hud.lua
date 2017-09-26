3c4440b01d7452ae89f776305fa95ef36516a89c
DBot <roboderpy@gmail.com>
2017-09-26 20:49:35 +0700

    Improved weapon stats display

--- weaponrystats/lua/autorun/weaponrystats/cl_hud.lua
+++ weaponrystats/lua/autorun/weaponrystats/cl_hud.lua
@@ -17,7 +17,13 @@ local weaponrystats = weaponrystats
 
 surface.CreateFont('WPS.DisplayName', {
 	font = 'Roboto',
-	size = 16,
+	size = 18,
+	weight = 400
+})
+
+surface.CreateFont('WPS.DisplayStats', {
+	font = 'Roboto',
+	size = 14,
 	weight = 400
 })
 
@@ -37,6 +43,18 @@ local qualityColors = {
 	Color(108, 0, 255),
 }
 
+local function doDrawText(text, x, y, color)
+	surface.SetTextColor(0, 0, 0)
+	surface.SetTextPos(x + 1, y + 1)
+	surface.DrawText(text)
+
+	surface.SetTextColor(color)
+	surface.SetTextPos(x, y)
+	surface.DrawText(text)
+end
+
+local STATS_COLOR = Color(255, 255, 255)
+
 local function HUDPaint()
 	local weapon = LocalPlayer():GetActiveWeapon()
 	if not IsValid(weapon) then return end
@@ -46,6 +64,12 @@ local function HUDPaint()
 	local x, y = ScrW() * 0.7, 15
 	local currentQuality = 0
 
+	local speed = 1
+	local force = 1
+	local damage = 1
+	local additional = false
+	local add = 0
+
 	if string.sub(name, 1, 1) == '#' then
 		name = language.GetPhrase(string.sub(name, 2))
 	end
@@ -53,23 +77,49 @@ local function HUDPaint()
 	if modif then
 		name = modif.name .. ' ' .. name
 		currentQuality = currentQuality + (modif.quality or 0)
+		speed = speed * modif.speed
+		force = force * modif.force
+		damage = damage * modif.damage
 	end
 
 	if wtype then
 		name = name .. ' of ' .. wtype.name
 		currentQuality = currentQuality + (wtype.quality or 0)
+		speed = speed * wtype.speed
+		force = damage * wtype.force
+
+		if wtype.isAdditional then
+			add = wtype.damage
+			additional = true
+		else
+			damage = damage * wtype.damage
+		end
 	end
 
 	currentQuality = math.Clamp(currentQuality + 2, 1, #qualityColors)
 	surface.SetFont('WPS.DisplayName')
 
-	surface.SetTextColor(0, 0, 0)
-	surface.SetTextPos(x + 1, y + 1)
-	surface.DrawText(name)
+	doDrawText(name, x, y, qualityColors[currentQuality])
 
-	surface.SetTextColor(qualityColors[currentQuality])
-	surface.SetTextPos(x, y)
-	surface.DrawText(name)
+	y = y + 17
+
+	surface.SetFont('WPS.DisplayStats')
+
+	doDrawText(string.format('Level %i weapon', currentQuality - 2), x, y, qualityColors[currentQuality])
+	y = y + 19
+
+	if additional then
+		doDrawText(string.format('%i%% damage (+%i%% additional damage)', damage * 100, add * 100), x, y, STATS_COLOR)
+		y = y + 15
+	else
+		doDrawText(string.format('%i%% damage', damage * 100), x, y, STATS_COLOR)
+		y = y + 15
+	end
+
+	doDrawText(string.format('%i%% attack speed', speed * 100), x, y, STATS_COLOR)
+	y = y + 15
+	doDrawText(string.format('%i%% knockback', force * 100), x, y, STATS_COLOR)
+	y = y + 15
 end
 
 weaponrystats.HUDPaint = HUDPaint
