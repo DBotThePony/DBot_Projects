37f2ca33335a7e486b3dd576735331576385b359
DBot <roboderpy@gmail.com>
2017-10-08 19:36:02 +0700

    Added workshop stuff

---
+++ weaponrystats.bat
@@ -0,0 +1,3 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe weaponrystats
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id  -addon weaponrystats.gma -icon weaponrystats.jpg -changes "See https://git.dbot.serealia.ca/dbot/dbot_projects"
+rm weaponrystats.gma
