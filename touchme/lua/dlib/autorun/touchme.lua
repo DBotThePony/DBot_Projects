93ed21e3144f072637b6056917470f5ba48118c8
DBotThePony <dbotthepony@yandex.ru>
2017-11-14 18:33:01 +0700

    Added proper Touch Me

--- touchme/lua/dlib/autorun/touchme.lua
+++ touchme/lua/dlib/autorun/touchme.lua
@@ -13,9 +13,36 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+local ENABLED = DLib.util.CreateSharedConvar('sv_touchme_enabled', '1', 'Enable Touch Me serverside')
+
+DLib.friends.Register('touchme', 'Touch me~', true)
+
 local function PhysgunPickup(ply, ent)
 	if not ent:IsPlayer() then return end
-	if ent:IsFriend(ply) then return true end
+	if ent:InVehicle() then return end
+
+	local mv = ent:GetMoveType()
+	if mv ~= MOVETYPE_WALK and mv ~= MOVETYPE_NOCLIP then return end
+
+	if ply:GetInfoBool('cl_touchme_enabled', true) and ent:GetInfoBool('cl_touchme_enabled', true) and ent:CheckDLibFriendIn(ply, 'touchme') then
+		if SERVER then
+			ent.__TouchMeStatus = true
+			ent:SetMoveType(MOVETYPE_NONE)
+		end
+
+		return true
+	end
 end
 
 hook.Add('PhysgunPickup', 'TouchMe', PhysgunPickup)
+
+if SERVER then
+	local function PhysgunDrop(ply, ent)
+		if not ent:IsPlayer() then return end
+		if not ent.__TouchMeStatus then return end
+		ent.__TouchMeStatus = false
+		ent:SetMoveType(MOVETYPE_WALK)
+	end
+
+	hook.Add('PhysgunDrop', 'TouchMe', PhysgunDrop)
+end
