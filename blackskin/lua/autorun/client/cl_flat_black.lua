1ef877960fc62c8d4e4a4961293d865a0ce56cb2
DBotThePony <dbotthepony@yandex.ru>
2017-11-14 20:36:49 +0700

    Fixed lua errors

--- blackskin/lua/autorun/client/cl_flat_black.lua
+++ blackskin/lua/autorun/client/cl_flat_black.lua
@@ -13,6 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+_G.DFlatBlack = _G.DFlatBlack or {}
 local FontIsOverrided = false
 
 local function OverrideFont(status)
