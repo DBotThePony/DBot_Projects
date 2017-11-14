b1685bbd5f8311161b680275623df592c5275762
DBotThePony <dbotthepony@yandex.ru>
2017-11-14 20:13:32 +0700

    Added workshop stuff

---
+++ touchme.bat
@@ -0,0 +1,3 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe touchme
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe create -addon touchme.gma -icon touchme.jpg -changes "See https://git.dbot.serealia.ca/dbot/dbot_projects"
+rm touchme.gma
