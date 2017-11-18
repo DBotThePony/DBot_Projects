d956e33b1b9f6c5e9c96b7cd242ce6a052bbb835
DBotThePony <dbotthepony@yandex.ru>
2017-11-18 11:02:36 +0700

    Added all basic indicators

--- gfreakmanhud/lua/dlib/autorun/client/freakmanhud.lua
+++ gfreakmanhud/lua/dlib/autorun/client/freakmanhud.lua
@@ -26,12 +26,20 @@ local ScrW = ScrW
 local ScrH = ScrH
 
 local ENABLE = CreateConVar('cl_freakman_hud', '1', {FCVAR_ARCHIVE}, 'Enable Gordon Freakman HUD')
+
 local pattern = HUDCommons.Pattern(true, 'GordonFreakman_SANITY', 24, -1.5, 1.5)
 local pattern2 = HUDCommons.Pattern(true, 'GordonFreakman_ALT', 24, -1.5, 1.5)
+local patternAmmo1 = HUDCommons.Pattern(true, 'GordonFreakman_AMMO1', 24, -1.5, 1.5)
+local patternAmmo2 = HUDCommons.Pattern(true, 'GordonFreakman_AMMO2', 24, -1.5, 1.5)
+local patternArmor = HUDCommons.Pattern(true, 'GordonFreakman_ARMOR', 24, -1.5, 1.5)
+
 local rainbow = DLib.Rainbow(64, 0.4, 2, true, 0.5)
 local rainbow2 = DLib.Rainbow(80, 0.25, 2, true, 0.5)
+local rainbowAmmo1 = DLib.Rainbow(100, 0.3, 2, true, 0.5)
+local rainbowAmmo2 = DLib.Rainbow(150, 0.4, 2, true, 0.5)
+local rainbowArmor = DLib.Rainbow(60, 0.3, 2, true, 0.5)
 
-local BACKGROUND_COLOR = Color(0, 0, 0, 90)
+local BACKGROUND_COLOR = Color(0, 0, 0, 150)
 local TEXT_COLOR = Color(228, 230, 68)
 local TEXT_COLOR_ALPHA = Color(228, 230, 68)
 
@@ -69,7 +77,7 @@ surface.CreateFont('Freakman_HudNumbersGlow_SMALL', {
 })
 
 local FONT_NUMBERS = 'Freakman_HudNumbers'
-local FONT_NUMBERSS_SMALL = 'Freakman_HudNumbersSmall'
+local FONT_NUMBERSS_SMALL = 'Freakman_HudNumbers_SMALL'
 local FONT = 'HudHintTextLarge'
 local FONT_NUMBERS_GLOWING = 'Freakman_HudNumbersGlow'
 local FONT_NUMBERS_GLOWING_SMALL = 'Freakman_HudNumbersGlow_SMALL'
@@ -78,12 +86,23 @@ local FIRST_THINK = false
 
 local HEALTH = 0
 local LAST_HEALTH_CHANGE = 0
+
+local ARMOR = 0
+local LAST_ARMOR_CHANGE = 0
+
 local CLIP1 = 0
 local CLIP2 = 0
 local CLIP1_MAX = 0
 local CLIP2_MAX = 0
 local AMMO1 = 0
 local AMMO2 = 0
+
+local CLIP1_CHANGE = 0
+local CLIP2_CHANGE = 0
+local CLIP1_MAX_CHANGE = 0
+local CLIP2_MAX_CHANGE = 0
+local AMMO1_CHANGE = 0
+local AMMO2_CHANGE = 0
 
 local function HUDPaint()
 	if not ENABLE:GetBool() then return end
@@ -112,6 +131,25 @@ local function HUDPaint()
 		pattern:SimpleText(HEALTH, FONT_NUMBERS_GLOWING, x, y, TEXT_COLOR_ALPHA)
 	end
 
+	if ARMOR > 0 then
+		patternArmor:Next()
+
+		y = ScrH() - 30 - HEALTH_HEIGHT
+		x = 80 + HEALTH_WIDTH
+
+		draw.RoundedBox(8, x, y, HEALTH_WIDTH, HEALTH_HEIGHT, BACKGROUND_COLOR)
+
+		x = x + 19
+		y = y + HEALTH_HEIGHT - 30
+
+		HUDCommons.SimpleText('HARDNESS', FONT, x, y, TEXT_COLOR)
+
+		x = x + 80
+		y = y - 45
+
+		patternArmor:SimpleText(ARMOR, FONT_NUMBERS, x, y, rainbowArmor:Next())
+	end
+
 	x, y = ScrW() - 40, ScrH() - 30 - HEALTH_HEIGHT
 
 	if CLIP2_MAX > 0 or CLIP2 > 0 or AMMO2 > 0 then
@@ -129,10 +167,15 @@ local function HUDPaint()
 
 	x = x - AMMO_WIDTH
 
-	if CLIP1_MAX > 0 or CLIP1 > 0 then
-		pattern:Next()
+	if CLIP1_MAX > 0 or CLIP1 > 0 or AMMO1 > 0 then
+		patternAmmo1:Next()
+		patternAmmo2:Next()
 
 		draw.RoundedBox(8, x, y, AMMO_WIDTH, HEALTH_HEIGHT, BACKGROUND_COLOR)
+
+		HUDCommons.SimpleText('AMMO', FONT, x + 20, y + 45, TEXT_COLOR)
+		patternAmmo1:SimpleText(CLIP1, FONT_NUMBERS, x + 90, y + 4, rainbowAmmo1:Next())
+		patternAmmo2:SimpleText(AMMO1, FONT_NUMBERSS_SMALL, x + 220, y + 34, rainbowAmmo2:Next())
 	end
 end
 
@@ -144,29 +187,58 @@ local function Tick()
 
 	local newhp = ply:Health()
 	local weapon = ply:GetActiveWeapon()
+	local _ARMOR = ply:Armor()
 
 	if HEALTH ~= newhp then
 		LAST_HEALTH_CHANGE = RealTime() + 2
 	end
 
+	if ARMOR ~= _ARMOR then
+		LAST_ARMOR_CHANGE = RealTime() + 2
+	end
+
 	HEALTH = newhp
+	ARMOR = _ARMOR
 
 	if IsValid(weapon) then
-		CLIP1 = weapon:Clip1()
-		CLIP2 = weapon:Clip2()
-		CLIP1_MAX = weapon:GetMaxClip1()
-		CLIP2_MAX = weapon:GetMaxClip2()
+		local _CLIP1 = weapon:Clip1()
+		local _CLIP2 = weapon:Clip2()
+		local _CLIP1_MAX = weapon:GetMaxClip1()
+		local _CLIP2_MAX = weapon:GetMaxClip2()
 
-		local AmmoID1 = weapon:GetPrimaryAmmoType()
-		local AmmoID2 = weapon:GetSecondaryAmmoType()
+		AMMO1 = ply:GetAmmoCount(weapon:GetPrimaryAmmoType())
+		AMMO2 = ply:GetAmmoCount(weapon:GetSecondaryAmmoType())
 
-		AMMO1 = ply:GetAmmoCount(AmmoID1)
-		AMMO2 = ply:GetAmmoCount(AmmoID2)
+		if CLIP1 ~= _CLIP1 then
+			CLIP1_CHANGE = RealTime() + 2
+		end
+
+		if CLIP2 ~= _CLIP2 then
+			CLIP2_CHANGE = RealTime() + 2
+		end
+
+		if CLIP1_MAX ~= _CLIP1_MAX then
+			CLIP1_MAX_CHANGE = RealTime() + 2
+		end
+
+		if CLIP2_MAX ~= _CLIP2_MAX then
+			CLIP2_MAX_CHANGE = RealTime() + 2
+		end
+
+		CLIP1 = _CLIP1
+		CLIP2 = _CLIP2
+		CLIP1_MAX = _CLIP1_MAX
+		CLIP2_MAX = _CLIP2_MAX
 	else
 		CLIP1 = 0
 		CLIP2 = 0
 		CLIP1_MAX = 0
 		CLIP2_MAX = 0
+
+		CLIP1_CHANGE = 0
+		CLIP2_CHANGE = 0
+		CLIP1_MAX_CHANGE = 0
+		CLIP2_MAX_CHANGE = 0
 	end
 end
 
@@ -175,6 +247,7 @@ local function HUDShouldDraw(elem)
 
 	local reply = elem == 'CHudHealth' or
 		elem == 'CHudSecondaryAmmo' or
+		elem == 'CHudBattery' or
 		elem == 'CHudAmmo'
 
 	if reply then return false end
