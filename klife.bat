e389ff1ea7c574c71bea32e03636e7f7589bbe25
DBotThePony <dbotthepony@yandex.ru>
2017-12-02 10:25:24 +0700

    Added workshop stuff

---
+++ klife.bat
@@ -0,0 +1,3 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe klife
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1218315674 -addon klife.gma -icon klife.jpg -changes "See https://git.dbot.serealia.ca/dbot/dbot_projects"
+rm klife.gma
