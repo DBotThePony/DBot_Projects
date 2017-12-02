d46c8d524e3b4ff3d0ba8c9123d01830c7cd274a
DBotThePony <dbotthepony@yandex.ru>
2017-12-02 10:31:56 +0700

    Add workshop download for fonts

--- klife/lua/dlib/autorun/server/kleinerlifehud.lua
+++ klife/lua/dlib/autorun/server/kleinerlifehud.lua
@@ -14,3 +14,5 @@
 -- limitations under the License.
 
 CreateConVar('sv_klife_hud', '1', {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_NOTIFY}, 'Enable Gordon Freakman HUD')
+
+resource.AddWorkshop('1218315674')
