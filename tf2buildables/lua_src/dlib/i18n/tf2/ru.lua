05abf75da1c80371b6d5f18591f9a00a18995105
DBotThePony <dbotthepony@yandex.ru>
2018-05-01 15:28:52 +0700

    ruski translation for tf2 buildables

---
+++ tf2buildables/lua_src/dlib/i18n/tf2/ru.lua
@@ -0,0 +1,42 @@
+
+-- Copyright (C) 2017-2018 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- DLib.i18n.localize
+
+gui.tf2.hud.sentry.bullets = 'Пули: %i/%i\n'
+gui.tf2.hud.sentry.rockets = 'Ракеты: %i/%i\n'
+gui.tf2.hud.generic.metal = 'Металл: %i'
+
+gui.tf2.hud.buildable.by = ' построено: %s'
+gui.tf2.hud.buildable.hp = 'Здоровье: %s'
+gui.tf2.hud.buildable.upgrade = 'Улучшение: %s'
+
+gui.tf2.hud.draw.sentry = 'ТУРЕЛЬ %i'
+gui.tf2.hud.draw.dispenser = 'РАЗДАТЧИК %i'
+gui.tf2.hud.draw.telein = 'ВХОД ТЕЛЕПОРТА %i'
+gui.tf2.hud.draw.teleout = 'ВЫХОД ТЕЛЕПОРТА %i'
+
+gui.tf2.hud.main.sentry = 'ТУРЕЛЬ'
+gui.tf2.hud.main.dispenser = 'РАЗДАТЧИК'
+gui.tf2.hud.main.telein = 'ВХОД ТЕЛЕПОРТА'
+gui.tf2.hud.main.teleout = 'ВЫХОД ТЕЛЕПОРТА'
+
+gui.tf2.hud.main.demolish = 'ВЗОРВАТЬ'
+gui.tf2.hud.main.build = 'ПОСТРОИТЬ'
+gui.tf2.hud.main.notbuilt = 'Не возведено'
+gui.tf2.hud.main.already = 'УЖЕ'
+gui.tf2.hud.main.notb = 'НЕ'
+gui.tf2.hud.main.built = 'ВОЗВЕДЕНО'
+gui.tf2.hud.main.cost = 'МЕТАЛЛ: %i'
