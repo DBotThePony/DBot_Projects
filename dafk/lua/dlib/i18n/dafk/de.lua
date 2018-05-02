8ff6c5ffd7c0726dd0b79f6cb26d64af77258865
FarukGamer <farukgamer14@gmail.com>
2018-05-02 14:15:43 +0200

    German Translation for DAFK

---
+++ dafk/lua/dlib/i18n/dafk/de.lua
@@ -0,0 +1,32 @@
+
+-- Copyright (C) 2016-2018 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+message.dafk.status.now = ' ist jetzt '
+message.dafk.status.back = 'ZURÜCK'
+message.dafk.status.away = 'WEG'
+message.dafk.status.from = ' von seiner Tastatur!'
+message.dafk.status.to = ' zu seiner Tastatur! War AFK für %s'
+
+player.dafk.status.tabbed = 'Rausgetabbt - Spielfenster ist nicht fokussiert'
+player.dafk.status.afk = 'AFK für: %s'
+player.dafk.status.awayfor = 'WEG FÜR'
+
+gui.dafk.menu.about = 'DAFK wurde erstellt von DBotThePony'
+gui.dafk.menu.cl_dafk_zzz = 'Zeichne "Zzz" effekt'
+gui.dafk.menu.cl_dafk_screen = 'AFK Zeit auf dem Bildschirm'
+gui.dafk.menu.cl_dafk_chat = 'Chat Benachrichtigungen aktivieren'
+gui.dafk.menu.cl_dafk_text = 'Text über Spielers Kopf zeichnen'
+gui.dafk.menu.cl_dafk_hide = 'DAFK ausblenden wenn die Kamera eingesetzt wird'
+gui.dafk.menu.cl_dafk_focus = 'Zeige an wann immer dein Spiel Fokussiert ist\nfür die anderen Spieler'
