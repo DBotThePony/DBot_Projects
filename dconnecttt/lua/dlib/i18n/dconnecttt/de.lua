a9d2671267c7799cef66d1f8782065876893227b
FarukGamer <farukgamer14@gmail.com>
2018-05-02 13:59:33 +0200

    German Translation for DConnecttt

---
+++ dconnecttt/lua/dlib/i18n/dconnecttt/de.lua
@@ -0,0 +1,35 @@
+
+-- Copyright (C) 2016-2018 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+message.dconn.player.connected = '%s ist mit dem Server verbunden'
+message.dconn.player.authed = ' ist authentifiziert'
+message.dconn.kick.spam = '%s wurde wegen Verbindungs Spam gekickt.'
+message.dconn.connected.first = ' ist zum ersten Mal dem Server beigetreten'
+message.dconn.connected.join = ' ist dem Server beigetreten'
+message.dconn.connected.lastseen = ' wurde zuletzt gesehen am %s'
+
+message.dconn.disconnected.disconnected = ' vom Server getrennt'
+message.dconn.disconnected.crash = 'Absturz/Netzwerk Problem'
+message.dconn.disconnected.noreply = '[DConnecttt] Dein Client hat eine Anfrage nicht rechtzeitig beantwortet. Bitte verbinde dich neu oder starte das Spiel neu.'
+
+gui.dconn.session.playerinfo = '\n\n%s\'s Sitzungszeit: %s\n%s\'s Gesamtzeit: %s'
+gui.dconn.session.info = 'Sitzungszeit:%s\nGesamtzeit: %s'
+
+gui.dconn.settings.cl_dconn_draw = 'Zeige einen visuellen Effekt wenn der Spieler die Verbindung verliert'
+gui.dconn.settings.cl_dconn_drawtime = 'Gezeigte Spielzeit auf den Server'
+gui.dconn.settings.cl_dconn_x = 'X Position vom text'
+gui.dconn.settings.cl_dconn_y = 'Y Position vom text'
+
+info.dconn.connection_lost = '? Verbindung verloren'
