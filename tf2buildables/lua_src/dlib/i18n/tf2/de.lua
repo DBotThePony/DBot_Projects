b9705529d30c8dfbcf7dd68f7352f9569a463ffc
FarukGamer <farukgamer14@gmail.com>
2018-05-02 14:05:45 +0200

    German Translation for DTF2

---
+++ tf2buildables/lua_src/dlib/i18n/tf2/de.lua
@@ -0,0 +1,42 @@
+
+-- Copyright (C) 2017-2018 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- DLib.i18n.localize
+
+gui.tf2.hud.sentry.bullets = 'Kugeln: %i/%i\n'
+gui.tf2.hud.sentry.rockets = 'Raketen: %i/%i\n'
+gui.tf2.hud.generic.metal = 'Verfügbares Metall: %i'
+
+gui.tf2.hud.buildable.by = ' gebaut von: %s'
+gui.tf2.hud.buildable.hp = 'Gesundheit: %s'
+gui.tf2.hud.buildable.upgrade = 'Upgrade level: %s'
+
+gui.tf2.hud.draw.sentry = 'SENTRY GUN %i'
+gui.tf2.hud.draw.dispenser = 'DISPENSER %i'
+gui.tf2.hud.draw.telein = 'TELEPORTER EINGANG %i'
+gui.tf2.hud.draw.teleout = 'TELEPORTER AUSGANG %i'
+
+gui.tf2.hud.main.sentry = 'SENTRY GUN'
+gui.tf2.hud.main.dispenser = 'DISPENSER'
+gui.tf2.hud.main.telein = 'TELEPORTER EINGANG'
+gui.tf2.hud.main.teleout = 'TELEPORTER AUSGANG'
+
+gui.tf2.hud.main.demolish = 'ZERSTÖREN'
+gui.tf2.hud.main.build = 'BAUEN'
+gui.tf2.hud.main.notbuilt = 'Nicht gebaut'
+gui.tf2.hud.main.already = 'BEREITS'
+gui.tf2.hud.main.notb = 'NICHT'
+gui.tf2.hud.main.built = 'GEBAUT'
+gui.tf2.hud.main.cost = 'KOSTEN: %i'
