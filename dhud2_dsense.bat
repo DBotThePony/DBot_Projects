b0e20a852de7644fcc8ad99b3f5025f3cdeff989
DBotThePony <dbotthepony@yandex.ru>
2018-06-18 15:32:44 +0700

    Damage numbers/sense workshop stuffs

---
+++ dhud2_dsense.bat
@@ -0,0 +1,3 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe dhud2_dsense
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1406507361 -addon dhud2_dsense.gma -icon dhud2_dsense.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
+rm dhud2_dsense.gma
