dbb1b5be2245d92231e8bac2f8dfcf90abb58043
DBotThePony <dbotthepony@yandex.ru>
2018-06-29 15:27:04 +0700

    Addef fastdl

---
+++ dvisuals/lua/dlib/autorun/server/dvisuals.lua
@@ -0,0 +1,75 @@
+
+-- Enhanced Visuals for GMod
+-- Copyright (C) 2018 DBot
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+local A = resource.AddSingleFile
+
+A'materials/enchancedvisuals/overlay/crack/crack0.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack1.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack10.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack11.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack12.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack2.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack3.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack4.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack5.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack6.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack7.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack8.vtf'
+A'materials/enchancedvisuals/overlay/crack/crack9.vtf'
+A'materials/enchancedvisuals/overlay/damaged/damaged0.vtf'
+A'materials/enchancedvisuals/overlay/freeze/freeze0.vtf'
+A'materials/enchancedvisuals/overlay/heat/heat0.vtf'
+A'materials/enchancedvisuals/overlay/lava/lava0.vtf'
+A'materials/enchancedvisuals/overlay/lowhealth/lowhealth0.vtf'
+A'materials/enchancedvisuals/overlay/potion/potion0.vtf'
+A'materials/enchancedvisuals/overlay/water/water0.vtf'
+A'materials/enchancedvisuals/splat/dust/dust0.vtf'
+A'materials/enchancedvisuals/splat/dust/dust1.vtf'
+A'materials/enchancedvisuals/splat/dust/dust2.vtf'
+A'materials/enchancedvisuals/splat/fire/fire0.vtf'
+A'materials/enchancedvisuals/splat/fire/fire1.vtf'
+A'materials/enchancedvisuals/splat/impact/impact0.vtf'
+A'materials/enchancedvisuals/splat/impact/impact1.vtf'
+A'materials/enchancedvisuals/splat/impact/impact2.vtf'
+A'materials/enchancedvisuals/splat/impact/impact3alphatest.vtf'
+A'materials/enchancedvisuals/splat/pierce/pierce0.vtf'
+A'materials/enchancedvisuals/splat/pierce/pierce1.vtf'
+A'materials/enchancedvisuals/splat/pierce/pierce2.vtf'
+A'materials/enchancedvisuals/splat/sand/sand0.vtf'
+A'materials/enchancedvisuals/splat/sand/sand1.vtf'
+A'materials/enchancedvisuals/splat/sand/sand2.vtf'
+A'materials/enchancedvisuals/splat/sand/sand3.vtf'
+A'materials/enchancedvisuals/splat/sand/sand4.vtf'
+A'materials/enchancedvisuals/splat/sand/sand5.vtf'
+A'materials/enchancedvisuals/splat/slash/slash0.vtf'
+A'materials/enchancedvisuals/splat/slash/slash1.vtf'
+A'materials/enchancedvisuals/splat/slash/slash2.vtf'
+A'materials/enchancedvisuals/splat/slash/slash3.vtf'
+A'materials/enchancedvisuals/splat/slash/slash4.vtf'
+A'materials/enchancedvisuals/splat/snow/snow0.vtf'
+A'materials/enchancedvisuals/splat/snow/snow1.vtf'
+A'materials/enchancedvisuals/splat/snow/snow2.vtf'
+A'materials/enchancedvisuals/splat/snow/snow3.vtf'
+A'materials/enchancedvisuals/splat/splatter/splatter0.vtf'
+A'materials/enchancedvisuals/splat/splatter/splatter1.vtf'
+A'materials/enchancedvisuals/splat/splatter/splatter2.vtf'
+A'materials/enchancedvisuals/splat/splatter/splatter3.vtf'
+A'materials/enchancedvisuals/splat/water/water0.vtf'
+A'materials/enchancedvisuals/splat/water/water3.vtf'
+A'sound/enhancedvisuals/heartbeatin.wav'
+A'sound/enhancedvisuals/heartbeatout.wav'
+A'sound/enhancedvisuals/ringing.wav'
