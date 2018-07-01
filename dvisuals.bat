28fbb519e9bc7d30a0452039ab1c640194b03dbd
DBotThePony <dbotthepony@yandex.ru>
2018-07-01 13:05:40 +0700

    Workshop stuff

---
+++ dvisuals.bat
@@ -0,0 +1,3 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe dvisuals
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1427986071 -addon dvisuals.gma -icon dvisuals.jpg -changes "See https://gitlab.com/DBotThePony/FFGSHUD"
+rm dvisuals.gma
