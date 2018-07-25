3546fc4d1f10ac0bd8e13a76929d12f759bca4ee
DBotThePony <dbotthepony@yandex.ru>
2018-07-25 20:45:23 +0700

    Don't pause

--- textscreen.bat
+++ textscreen.bat
@@ -1,4 +1,3 @@
 E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe textscreen
 E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1451470222 -addon textscreen.gma -icon textscreen.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
-PAUSE
 rm textscreen.gma
