ca843f998187207340eb767e32b80cfdd0dd886e
DBotThePony <dbotthepony@yandex.ru>
2018-07-25 20:21:28 +0700

    Workshop stuffs

---
+++ textscreen.bat
@@ -0,0 +1,4 @@
+E:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe textscreen
+E:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1451470222 -addon textscreen.gma -icon textscreen.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
+PAUSE
+rm textscreen.gma
