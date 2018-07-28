44b3e58f26a462d1a695b4296be95a357840c8ec
DBotThePony <dbotthepony@yandex.ru>
2018-07-28 08:46:57 +0700

    Use tostring

--- daynight/lua/ddaynight/common/time_date.lua
+++ daynight/lua/ddaynight/common/time_date.lua
@@ -35,7 +35,7 @@ function meta:Initialize(stamp)
 end
 
 function meta:SetStamp(stamp)
-	assert(type(stamp) == 'number' and stamp >= 0, 'Invalid stamp. Called time is ' .. stamp)
+	assert(type(stamp) == 'number' and stamp >= 0, 'Invalid stamp. Called time is ' .. tostring(stamp))
 	self.stamp = stamp
 
 	if not self.dayObject or self.dayObject:GetAbsoluteDay() ~= self:GetAbsoluteDay() then
