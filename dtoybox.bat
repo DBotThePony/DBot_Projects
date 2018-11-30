91ee512b5ebd0c6de149f1c9667e4b7d49f12d3b
DBotThePony <dbotthepony@yandex.ru>
2018-11-30 18:56:03 +0700

    Workshop stuff

---
+++ dtoybox.bat
@@ -0,0 +1,3 @@
+F:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe dtoybox
+F:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1578347355 -addon dtoybox.gma -icon dtoybox.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
+rm dtoybox.gma
