f35fdaad0308410d24a748d1792b639cf1602fff
DBotThePony <dbotthepony@yandex.ru>
2019-05-08 09:10:19 +0700

    rename

--- limitedflandoxygen/lua/dlib/i18n/hev/en.lua
+++ limitedflandoxygen/lua/dlib/i18n/hev/en.lua
@@ -2,5 +2,5 @@
 -- The Unlicense (no Copyright) DBotThePony
 -- do whatever you want, including removal of this notice
 
-gui.breathe.oxygen = 'Кислород'
-gui.breathe.flashlight = 'Фонарик'
+gui.hev.oxygen = 'Oxygen'
+gui.hev.flashlight = 'Flashlight'
--- limitedflandoxygen/lua/dlib/i18n/hev/ru.lua
+++ limitedflandoxygen/lua/dlib/i18n/hev/ru.lua
@@ -2,5 +2,5 @@
 -- The Unlicense (no Copyright) DBotThePony
 -- do whatever you want, including removal of this notice
 
-gui.breathe.oxygen = 'Oxygen'
-gui.breathe.flashlight = 'Flashlight'
+gui.hev.oxygen = 'Кислород'
+gui.hev.flashlight = 'Фонарик'
