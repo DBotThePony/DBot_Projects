cb7fc617250ec4b665379a197279d67a5edc1c23
DBotThePony <dbotthepony@yandex.ru>
2019-09-27 20:51:55 +0700

    workshop stuffs

---
+++ mcdeaths.bat
@@ -0,0 +1,3 @@
+F:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe mcdeaths
+F:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe create -addon mcdeaths.gma -icon mcdeaths.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
+rm mcdeaths.gma
