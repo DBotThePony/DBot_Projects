aeb5c68b7046fd4604f92545cc0445e32741246b
DBotThePony <dbotthepony@yandex.ru>
2019-09-27 21:07:30 +0700

    Update script

--- mcdeaths.bat
+++ mcdeaths.bat
@@ -1,3 +1,3 @@
 F:\Steam\SteamApps\common\GarrysMod\bin\gmad.exe mcdeaths
-F:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe create -addon mcdeaths.gma -icon mcdeaths.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
+F:\Steam\SteamApps\common\GarrysMod\bin\gmpublish.exe update -id 1873856710 -addon mcdeaths.gma -icon mcdeaths.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
 rm mcdeaths.gma
